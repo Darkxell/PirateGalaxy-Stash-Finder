8097e1b8749540320f511f981e2c89fdf2a418bf
CANDELA Nicolas <darkxell.mc@gmail.com>
2015-12-24 14:44:25 +0100

    Create Scann.java

---
+++ main/Scann.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Scann {
+
+    public int x;
+    public int y;
+    public int dist;
+    
+    public Scann(int x,int y,int dist){
+	this.x = x;
+	this.y = y;
+	this.dist = dist;
+    }
+}
